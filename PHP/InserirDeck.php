a230d0d8e9d3c388466f95c8c281122063980101
Maycon <maycon_ribeiro10@hotmail.com>
2023-03-14 21:25:47 -0300

    Att

---
+++ PHP/InserirDeck.php
@@ -0,0 +1,41 @@
+<?php
+
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Headers: *');
+    header('Access-Control-Allow-Methods: *');
+
+    require_once("Conectar.php");
+
+    use DAO\conectar;
+
+    $json = file_get_contents("php://input");
+    $obj = json_decode($json);
+
+
+    $pergunta = $obj['pergunta'];
+    $resposta = $obj['resposta'];
+
+
+
+
+    $conn = $conexao->conectar();
+    $sql = "insert into decks (idDeck, nomeDeck) values ('', '$nomeDeck')";
+    $result = mysqli_query($conn,$sql);
+
+    if($result){
+        echo json_encode("<br><br>Flashcard Adicionado!");
+        return;
+    }else{
+        echo json_encode("<br><br>Ops, Aconteceu um erro, tente novamente! :(");
+        return;
+    }//Fim if else
+
+    mysqli_close($conn);
+
+
+
+
+
+
+?>
